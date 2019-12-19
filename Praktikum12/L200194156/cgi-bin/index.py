02dbf78bbda19f7af736be011958092ee478218a
L200194156 <55620647+L200194156@users.noreply.github.com>
2019-12-19 19:46:53 +0700

    Delete index.py

--- Praktikum12/L200194156/cgi-bin/index.py
+++
@@ -1,9 +0,0 @@
-print("<!DOCTYPE html>\n")
-print("""<html>
-<head><title>Akses dengan SimpleHTTPRequestHadler</title></head>
-<body><h3>Data Diri Mahasiswa</h3>""")
-print("""<table><tr><td>Nama</td><td>:</td><td>Radin Munding</td></tr>
-		<tr><td>Tempat, tanggal lahir</td><td>:</td><td>Sukoharjo, 27 Desember 1999</td></tr>
-		<tr><td>Tempat wisata favorit</td><td>:</td><td>Pegunungan</td></tr>
-		<tr><td>Motto</td><td>:</td><td>Perbanyaklah waktu berkualitas</td></tr></table>""")
-print("</body></html>")
